4ce3a2271f4520a0ea7a3f170389f28130a6ce4c
Gregor Lenz <gregor.lenz@synsense.ai>
2022-02-13 22:05:26 +0100

    add first draft for rectification

--- quartz/layer.py
+++ quartz/layer.py
@@ -7,22 +7,31 @@ import torch.nn as nn
 class IF(sl.StatefulLayer):
     def __init__(
         self,
-        t_max: int = 128,
+        t_max: int,
+        index: int,
+        rectification: bool = True,
         record_v_mem: bool = False,
     ):
         super().__init__(state_names=["v_mem", "i_syn"])
         self.t_max = t_max
+        self.index = index
+        self.rectification = rectification
         self.act_fn = sina.ActivationFunction(spike_threshold=t_max, spike_fn=sina.SingleSpike, reset_fn=sina.MembraneReset())
         self.record_v_mem = record_v_mem
-        self.v_mem_recorded = []
 
     def forward(self, data: torch.Tensor) -> torch.Tensor:
         batch_size, n_time_steps, *trailing_dim = data.shape
+
+        self.v_mem_recorded = []
 
         if not self.is_state_initialised() or not self.state_has_shape(
             (batch_size, *trailing_dim)
         ):
             self.init_state_with_shape((batch_size, *trailing_dim))
+
+        counter_weight = torch.zeros_like(data)
+        counter_weight[:, self.t_max*(self.index+1)] = 1 - data.sum()
+        data = data + counter_weight
 
         output_spikes = []
         for step in range(n_time_steps):
@@ -34,7 +43,13 @@ class IF(sl.StatefulLayer):
             self.v_mem = state["v_mem"]
             self.i_syn[spikes.bool()] = 0
 
+            if self.rectification: 
             if self.record_v_mem: self.v_mem_recorded.append(self.v_mem)
         
         if self.record_v_mem: self.v_mem_recorded = torch.stack(self.v_mem_recorded, 1)
-        return torch.stack(output_spikes, 1)
+
+        spikes = torch.stack(output_spikes, 1)
+        if self.rectification and (spikes[:, :self.t_max*(self.index+2)].sum(1) == 0).any():
+            spikes[:, :self.t_max*(self.index+2)].sum(1) == 0 pass
+        
+        return spikes
