144965f44e7a247680e1c61acadc200904772687
Gregor Lenz <gregor.lenz@synsense.ai>
2022-04-17 17:24:28 +0200

    mnist experiment working

--- quartz/utils.py
+++ quartz/utils.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm.auto import tqdm
 
 
 def encode_inputs(data, t_max):
@@ -40,3 +41,21 @@ def quantize_parameters(weights, biases, weight_acc, t_max):
 
 def quantize_inputs(inputs, t_max):
     return (inputs * (t_max - 1)).round() / (t_max - 1)
+
+def get_accuracy(model, data_loader, device, t_max=None):
+    correct_pred = 0 
+    n = 0
+    model.eval()
+    for X, y_true in tqdm(data_loader):
+        X = X.to(device)
+        if t_max is not None:
+            X = encode_inputs(X, t_max=t_max).to(device)
+        y_true = y_true.to(device)
+        with torch.no_grad():
+            y_prob = model(X)
+        if t_max is not None:
+            y_prob = decode_outputs(y_prob, t_max=t_max)
+        _, predicted_labels = torch.max(y_prob, 1)
+        n += y_true.size(0)
+        correct_pred += (predicted_labels == y_true).sum()
+    return correct_pred.float() / n
