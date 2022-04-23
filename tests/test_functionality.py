c936fd836a6fa2fcdee648381b46fc517459e840
Gregor Lenz <gregor.lenz@synsense.ai>
2022-04-23 13:26:04 +0200

    bias tests passing again

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -77,21 +77,19 @@ def test_inputs(weight, value):
 def test_bias(bias):
     t_max = 2**8 + 1
     values = torch.zeros((1, 1))
+    q_bias = quartz.quantize_inputs(torch.tensor(bias).float(), t_max=t_max)
 
     linear_layer = nn.Linear(1, 1)
     linear_layer.weight = torch.nn.Parameter(torch.zeros_like(linear_layer.weight))
-    linear_layer.bias = torch.nn.Parameter(torch.ones_like(linear_layer.bias) * bias)
+    linear_layer.bias = torch.nn.Parameter(torch.ones_like(linear_layer.bias) * q_bias)
     ann_output = linear_layer(values)
     q_ann_output = quartz.quantize_inputs(ann_output, t_max)
 
     temp_q_values = quartz.encode_inputs(values, t_max=t_max)
-    temp_linear = linear_layer(temp_q_values.flatten(0, 1)).unflatten(0, (1, -1))
-    quartz_output = quartz.IF(t_max=t_max, rectification=False, bias=bias)(temp_linear)
+    temp_linear_layer = quartz.Repeat(linear_layer)
+    temp_linear = temp_linear_layer(temp_q_values)
+    quartz_output = quartz.IF(t_max=t_max, rectification=False)(temp_linear)
     q_quartz_output = quartz.decode_outputs(quartz_output, t_max=t_max)
-
-    print(
-        f"Ann output is {q_ann_output.item()}, snn output is {q_quartz_output.item()}."
-    )
 
     assert torch.all(q_ann_output == q_quartz_output)
 
@@ -102,15 +100,15 @@ def test_multi_bias():
 
     conv_layer = nn.Conv2d(5, 5, 1)
     conv_layer.weight = torch.nn.Parameter(torch.zeros_like(conv_layer.weight))
-    print(f"layer bias: {conv_layer.bias}")
+    conv_layer.bias.data = quartz.quantize_inputs(conv_layer.bias.data.clone(), t_max)
+    # print(f"layer bias: {conv_layer.bias}")
     ann_output = conv_layer(values)
     q_ann_output = quartz.quantize_inputs(ann_output, t_max)
 
     temp_q_values = quartz.encode_inputs(values, t_max=t_max)
-    temp_linear = conv_layer(temp_q_values.flatten(0, 1)).unflatten(0, (1, -1))
-    quartz_output = quartz.IF(t_max=t_max, rectification=False, bias=conv_layer.bias)(
-        temp_linear
-    )
+    temp_conv_layer = quartz.Repeat(conv_layer)
+    temp_conv = temp_conv_layer(temp_q_values)
+    quartz_output = quartz.IF(t_max=t_max, rectification=False)(temp_conv)
     q_quartz_output = quartz.decode_outputs(quartz_output, t_max=t_max)
 
     assert torch.all(q_ann_output == q_quartz_output)
